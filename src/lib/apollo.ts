cf13e10fa352954c8ddee90bbc0e887e33c03f0b
Shayane Teixeira <shayaneteixeira56@gmail.com>
2022-06-24 14:46:11 -0300

    aula 04: graphql

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pv6l7i29vc01xxbzog71uk/master',
-    cache: new InMemoryCache()
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
+  cache: new InMemoryCache(),
 })
